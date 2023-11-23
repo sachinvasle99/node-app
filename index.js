56fd5268a836399f6cc8b9839d95c42f0dd5b35d
mac <mac@macs-MacBook-Pro.local>
2023-11-23 13:10:14 +0530

    Add redis

--- index.js
+++ index.js
@@ -1,23 +1,31 @@
+const redis = require('redis');
 const express = require('express');
 const pg = require('pg');
 const bodyParser = require('body-parser');
 
-// Connect to PostgreSQL database using environment variables or default credentials
+// Connect to Redis server
+const client = redis.createClient({
+    host: process.env.REDIS_HOST || 'redis-master.redis.svc.cluster.local',
+    port: process.env.REDIS_PORT || 6379,
+    auth: process.env.REDIS_PASSWORD || 'sGz6AhXEAX',
+});
+
+// Connect to PostgreSQL database using environment variables
 const pool = new pg.Pool({
     user: process.env.DB_USER || 'postgres',
-    password: process.env.DB_PASSWORD || 'ECYLQKMJS71c2GUu13lY',
-    database: process.env.DB_DATABASE || 'postgres',
-    host: process.env.DB_HOST || 'database-1.cluster-cbfibzwafy6o.us-west-2.rds.amazonaws.com',
+    password: process.env.DB_PASSWORD || 'password',
+    database: process.env.DB_DATABASE || 'mydb',
+    host: process.env.DB_HOST || 'localhost',
     port: process.env.DB_PORT || 5432,
 });
 
 // Create `data` table if it doesn't exist
 (async () => {
     try {
-        await pool.query(`CREATE TABLE IF NOT EXISTS data (
+        await pool.query('CREATE TABLE IF NOT EXISTS data (
             id SERIAL PRIMARY KEY,
             value TEXT
-        )`);
+        )');
     } catch (error) {
         console.error(error);
     }
@@ -31,12 +39,34 @@ app.get('/status', (req, res) => {
     res.json({ status: 'OK' });
 });
 
+// GET /data
+app.get('/data', async (req, res) => {
+    const cachedData = await client.get('data');
+
+    if (cachedData) {
+        res.json({ message: 'Data retrieved from cache', data: cachedData });
+        return;
+    }
+
+    try {
+        const dataResult = await pool.query('SELECT value FROM data ORDER BY id DESC LIMIT 1');
+        const data = dataResult.rows[0].value;
+
+        await client.setex('data', 60, data); // Cache data for 60 seconds
+        res.json({ message: 'Data retrieved from database', data });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Failed to retrieve data' });
+    }
+});
+
 // POST /data
 app.post('/data', async (req, res) => {
     const { data } = req.body;
 
     try {
-        await pool.query('INSERT INTO data (value) VALUES ($1)', [data]);   
+        await pool.query('INSERT INTO data (value) VALUES ($1)', [data]);
+        await client.del('data'); // Clear cache since data has changed
         res.json({ message: 'Data stored successfully' });
     } catch (error) {
         console.error(error);
